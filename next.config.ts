a0e13fff8e8099783a3c5f8e0bdd0e3d0e32b9e8
Mudassir <mudassirdev3@gmail.com>
2026-05-05 21:50:48 +0500

    final deployment fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Ye line TypeScript errors ko ignore karegi taake build pass ho jaye
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ye line linting errors ko ignore karegi
     ignoreDuringBuilds: true,
   },
 };
